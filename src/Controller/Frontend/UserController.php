7d4928c75c70b2cf5705d529d7bad1d29678d83d
SebMarc <seb.marcaire@gmail.com>
2019-10-23 09:43:40 +0200

    Profil_User_Update V1

--- src/Controller/Frontend/UserController.php
+++ src/Controller/Frontend/UserController.php
@@ -6,6 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use App\Form\UserUpdateProfilType;
 
 class UserController extends AbstractController
 {
@@ -18,6 +22,50 @@ class UserController extends AbstractController
 
         return $this->render('frontend/user/profil.html.twig', [
             'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/profil/update/{id<\d+>}", name="profil_update")
+     * 
+     */
+    public function profilupdate(Request $request, User $user, UserPasswordEncoderInterface $passwordEncoder, ObjectManager $manager) {
+
+        // On stocke le mot de passe courant
+        $currentPassword = $user->getPassword();
+
+        $form = $this->createForm(UserUpdateProfilType::class, $user);
+        // Ici, le nouveau mot de passe sera renseigné dans $user
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Si le mot de passe n'est pas modifié, on conserve l'ancien
+            if (empty($user->getPassword())) {
+                $user->setPassword($currentPassword);
+            // Sinon, on encode le nouveau mot de passe
+            } else {
+                // Mot de passe qui vient du formulaire
+                $password = $user->getPassword();
+                // On l'encode via le passwordEncoder reçu depuis la méthode du contrôleur
+                $encodedPassword = $passwordEncoder->encodePassword($user, $password);
+                // On écrase le mot de passe avec le mot de passe encodé
+                $user->setPassword($encodedPassword);
+            }
+              
+            $manager->flush();
+
+            $this->addFlash(
+                    'profil modified',
+                    'Votre modification de profil a bien été enregistrée - Veuillez vous reconnecter'
+                );
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('frontend/user/profil_update.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
         ]);
     }
 }
