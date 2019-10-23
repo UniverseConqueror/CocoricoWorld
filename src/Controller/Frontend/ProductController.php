3d29d6592d886299594902bdf673a70f4fad2107
UniverseConqueror <adrillef@gmail.com>
2019-10-23 10:30:59 +0200

    add datas on productshow page

--- src/Controller/Frontend/ProductController.php
+++ src/Controller/Frontend/ProductController.php
@@ -1,21 +1,28 @@
 <?php
-
 namespace App\Controller\Frontend;
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Product;
+use App\Entity\Producer;
 use App\Repository\ProductRepository;
 
 class ProductController extends AbstractController
 {
-    /**
-     * @Route("/product/{id<\d+>}", name="product_show", methods={"GET"})
-     */
-    public function show(Product $product)
-    {
-        return $this->render('frontend/product/show.html.twig', [
-            'product' => $product
-        ]);
-    }
+   /**
+    * @Route("/product/{id<\d+>}", name="product_show", methods={"GET"})
+    */
+   public function show(Product $product, ProductRepository $productRepository)
+   {
+       $producer = $product->getProducer();
+       $products = $productRepository->findBy(array('producer' => $producer), array(), 5);
+
+       
+       if (!$product) {
+           throw $this->createNotFoundException('Producteur introuvable');
+       }
+       return $this->render('frontend/product/show.html.twig', [
+           'product' => $product,
+           'product_producer'=> $products
+       ]);
+   }
 }
