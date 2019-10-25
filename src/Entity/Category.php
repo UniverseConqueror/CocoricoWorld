0776c203c25a277e1f47e3c3cf9b306fed21f1e1
Eredost <michael.coutin62@gmail.com>
2019-10-25 23:50:22 +0200

    Add Subcategory admin dashboard

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -62,6 +62,11 @@ class Category
         $this->subcategories = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->name;
+    }
+
     public function getId(): ?int
     {
         return $this->id;
--- src/Repository/SubcategoryRepository.php
+++ src/Repository/SubcategoryRepository.php
@@ -5,6 +5,7 @@ namespace App\Repository;
 use App\Entity\Subcategory;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * @method Subcategory|null find($id, $lockMode = null, $lockVersion = null)
@@ -17,6 +18,22 @@ class SubcategoryRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Subcategory::class);
+    }
+
+    public function getAllWithSearchQueryBuilder($term): QueryBuilder
+    {
+        $qb = $this->createQueryBuilder('s')
+            ->innerJoin('s.category', 'c')
+            ->addSelect('c')
+        ;
+
+        if ($term) {
+            $qb->andWhere('s.name LIKE :term OR c.name LIKE :term')
+                ->setParameter('term', '%'.$term.'%')
+            ;
+        }
+
+        return $qb;
     }
 
     // /**
