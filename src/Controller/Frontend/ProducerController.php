bd5b3bd9ee7ee13ca5537c7ec9054aba0244e952
Eredost <michael.coutin62@gmail.com>
2019-11-01 11:03:01 +0100

    Add 403 error template with redirection to it

--- src/Controller/Frontend/ProducerController.php
+++ src/Controller/Frontend/ProducerController.php
@@ -10,11 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
-use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-// use Symfony\Component\HttpFoundation\File\Exception\FileException;
-// use Symfony\Component\HttpFoundation\File\Exception\AccessDeniedException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ProducerController extends AbstractController
@@ -34,8 +30,7 @@ class ProducerController extends AbstractController
             ]);
         }
 
-        throw new AccessDeniedException();
-    
+        throw $this->createAccessDeniedException();
     }
 
     /**
--- src/Security/AccessDeniedHandler.php
+++ src/Security/AccessDeniedHandler.php
@@ -3,17 +3,58 @@
 
 namespace App\Security;
 
+use App\Repository\UniversRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+use Twig\Environment;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
+    /** @var Environment */
+    private $environment;
+
+    /** @var UniversRepository */
+    private $universRepository;
+
+    /**
+     * AccessDeniedHandler constructor.
+     *
+     * @param Environment $environment
+     * @param UniversRepository $universRepository
+     */
+    public function __construct(Environment $environment, UniversRepository $universRepository)
+    {
+        $this->environment = $environment;
+        $this->universRepository = $universRepository;
+    }
+
+    /**
+     * Retrieves information related to the error and returns to the 403 template defined in the Exception folder
+     *
+     * if ($this->getUser()->getId() !== $id) {
+     *     throw new AccessDeniedException('You are not allowed to access this page');
+     * }
+     *
+     * @param  Request $request
+     * @param  AccessDeniedException $accessDeniedException
+     *
+     * @return Response|null
+     *
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-        $content = 'Vous ne pouvez pas acceder à cette page - Accès interdit';
+        $univers = $this->universRepository->findAllUniversWithCategoriesAndSubcategories();
+        $this->environment->addGlobal('universes', $univers);
+        dd($accessDeniedException);
 
-        return new Response($content, 403);
+        return new Response(
+            $this->environment->render('bundles/TwigBundle/Exception/error403.html.twig'),
+            403
+        );
     }
 }
